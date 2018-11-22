cafd9c5f22bf456a75899d0c5218cc4e56ac4e7b
Jan <mail@jezekjan.com>
2018-11-22 19:03:13 +0100

    user route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,4 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :users
   end
-  constraints subdomain: 'api' do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    namespace :v1 do
-      resources :users
-    end
-  end
 end
