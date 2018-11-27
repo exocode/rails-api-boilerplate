cf577aedb99b46eb58112bfdc3919a858c90c3ec
Jan <mail@jezekjan.com>
2018-11-27 17:56:42 +0100

    fixed Faker.name

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:user) do
   email    {Faker::Internet.email}
-  name    {Faker::Internet.name}
+  name    {Faker::Name.name}
   password {Faker::Internet.password}
   birthday Date.today - 18.years
 end
