4e250c4af692a768c33a22874bb4e846dbed6797
Jan <mail@jezekjan.com>
2019-10-16 11:47:05 +0200

    updated ruby version, refactored secrets from database.yml

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -13,7 +13,7 @@ class JsonWebToken
     # get payload; first index in decoded Array
     body = JWT.decode(token, HMAC_SECRET)[0]
     HashWithIndifferentAccess.new body
-      # rescue from all decode errors
+  # rescue from all decode errors
   rescue JWT::DecodeError => e
     # raise custom error to be handled by custom handler
     raise ExceptionHandler::InvalidToken, e.message
